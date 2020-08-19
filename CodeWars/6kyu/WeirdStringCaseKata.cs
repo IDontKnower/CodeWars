56ba06d551a7ba005ab68c52265d514bb17874f1
Alexey Bykov <79778742616@yandex.ru>
2020-08-19 14:02:57 +0300

    complete WeirdCaseKata

---
+++ CodeWars/6kyu/WeirdStringCaseKata.cs
@@ -0,0 +1,23 @@
+﻿// Reference: https://www.codewars.com/kata/52b757663a95b11b3d00062d
+
+using System;
+using System.Globalization;
+using System.Linq;
+
+namespace CodeWars.CodeWars._6kyu
+{
+    public class WeirdStringCaseKata
+    {
+        public static string ToWeirdCase(string s)
+        {
+            var textInfo = new CultureInfo("en-US",false).TextInfo;
+            var words = s.Split(" ");
+            var list = words.Select(word =>
+                word.Select((x, i) => i % 2 == 0 ? textInfo.ToUpper(x) : textInfo.ToLower(x)))
+                .Select(weirdCase => String.Join("", weirdCase))
+                .ToList();
+
+            return string.Join(" ", list);
+        }
+    }
+}
