69631eb16da318bd63b1a17c49acf4bcdbd6a910
Alexey Bykov <79778742616@yandex.ru>
2020-08-19 09:16:18 +0300

    remove usings from Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,10 @@
-﻿using System;
-
-namespace CodeWars
+﻿namespace CodeWars
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
