ccc170f4240e9b30f762bcc1b0d93a1fd249daf5
Alexey Bykov <79778742616@yandex.ru>
2020-08-19 13:47:20 +0300

    complete SimplePigLatinKata

---
+++ CodeWars/5kyu/SimplePigLatinKata.cs
@@ -0,0 +1,18 @@
+﻿// Reference: https://www.codewars.com/kata/520b9d2ad5c005041100000f
+
+using System;
+using System.Linq;
+
+namespace CodeWars.CodeWars._5kyu
+{
+    public class SimplePigLatinKata
+    {
+        public static string PigIt(string str)
+        {
+            return String.Join(' ',
+                str.Split(" ")
+                    .Select(word => word.Substring(1) + word.Substring(0, 1) + "ay")
+                );
+        }
+    }
+}
