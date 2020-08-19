c5d1077ed1645505c1bb26ad5675609932b13df2
Alexey Bykov <79778742616@yandex.ru>
2020-08-19 15:00:51 +0300

    complete Detectpangram kata

---
+++ CodeWars/6kyu/DetectPangramKata.cs
@@ -0,0 +1,22 @@
+﻿//Reference: https://www.codewars.com/kata/545cedaa9943f7fe7b000048
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars.CodeWars._6kyu
+{
+    public class DetectPangramKata
+    {
+        public static bool IsPangram(string str)
+        {
+            var lettersIndex = new HashSet<int>(Enumerable.Range(1, 26));
+            var lowerString = str.Replace(" ", "").ToLower();
+            foreach (var letter in lowerString.Where(letter => lettersIndex.Contains((int) letter - 96)))
+            {
+                lettersIndex.Remove((int) letter - 96);
+            }
+
+            return lettersIndex.Count == 0;
+        }
+    }
+}
