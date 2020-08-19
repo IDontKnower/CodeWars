71f818855de639850ff23497c2aefd0a2f5058e6
Alexey Bykov <79778742616@yandex.ru>
2020-08-19 10:34:15 +0300

    complete Highest word kata

---
+++ CodeWars/6kyu/HighestScoringWordKata.cs
@@ -0,0 +1,19 @@
+﻿// Reference: https://www.codewars.com/kata/57eb8fcdf670e99d9b000272/train/csharp
+
+using System.Linq;
+
+namespace CodeWars.CodeWars._6kyu
+{
+    public class HighestScoringWordKata
+    {
+        public static string High(string s)
+        {
+            var words = s.Split(" ");
+            var maxScore = words.Select(x => x.Sum(letter => (int) letter - 96)).Max();
+            var highest = words
+                .Where(x => x.Sum(letter => (int) letter - 96) == maxScore)
+                .ToList();
+            return highest.Count == 1 ? highest.First() : words.FirstOrDefault(word => highest.Contains(word));
+        }
+    }
+}
