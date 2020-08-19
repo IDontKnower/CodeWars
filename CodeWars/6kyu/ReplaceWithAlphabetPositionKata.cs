96f001c32cb737dc90f4e3cf9e6056d7eccf04b1
Alexey Bykov <79778742616@yandex.ru>
2020-08-19 15:35:13 +0300

    complete replace with alphabet position kata

---
+++ CodeWars/6kyu/ReplaceWithAlphabetPositionKata.cs
@@ -0,0 +1,16 @@
+﻿//Reference: https://www.codewars.com/kata/546f922b54af40e1e90001da
+
+using System.Linq;
+
+namespace CodeWars.CodeWars._6kyu
+{
+    public class ReplaceWithAlphabetPositionKata
+    {
+        public static string AlphabetPosition(string text)
+        {
+            var lowerText = text.ToLower();
+            var list = (from letter in lowerText where char.IsLetter(letter) select (int) letter - 96).ToList();
+            return string.Join(" ", list);
+        }
+    }
+}
