78ace0d8da7ae5b5795dc3a2ec09bef0ca4b4d28
Alexey Bykov <79778742616@yandex.ru>
2020-08-19 09:26:02 +0300

    complete CamelCaseKata

---
+++ CodeWars/6kyu/CamelCaseKata.cs
@@ -0,0 +1,16 @@
+﻿// Reference: https://www.codewars.com/kata/587731fda577b3d1b0001196
+
+using System.Globalization;
+using System.Linq;
+
+namespace CodeWars.CodeWars._6kyu
+{
+    public class CamelCaseKata
+    {
+        public static string CamelCase(string str)  
+        {  
+            var textInfo = new CultureInfo("en-US",false).TextInfo;
+            return string.Join("", str.Split(" ").Select(x => textInfo.ToTitleCase(x)));
+        }
+    }
+}
